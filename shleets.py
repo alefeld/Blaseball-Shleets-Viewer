a85edcdd31382401e65f06d55962ddfc364774b1
Anthony <alefeld@alumni.nd.edu>
2021-10-14 00:21:22 -0400

    Make Shleets loop! -Shleets now loops indefinitely! -Only seasons 12-23 because those are the only ones that work (was that tautological?) -Shleets also skips downtime between postseason rounds 2&3 and between the championship and the next season

--- shleets.py
+++ shleets.py
@@ -1,11 +1,39 @@
 import blaseball_mike.database as mikedb
 import asyncio
 import gspread
+import time
 
 
 credentials = gspread.service_account()
 spreadsheet_id = '1rDdGyhxELItkd82vjoN0mLJ9r2GGpeJCeRo33-gb0go'
 worksheet = credentials.open_by_key(spreadsheet_id).worksheet('All Data')
+
+timestamps = [
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-01T15:59:50.17Z&interval=4000', # Season 12 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-06T15:59:50.17Z&interval=4000', # Season 12 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-08T15:59:50.17Z&interval=4000', # Season 13 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-13T15:59:50.17Z&interval=4000', # Season 13 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-15T15:59:50.17Z&interval=4000', # Season 14 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-03-20T15:59:50.17Z&interval=4000', # Season 14 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-05T15:59:50.17Z&interval=4000', # Season 15 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-10T15:59:50.17Z&interval=4000', # Season 15 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-12T15:59:50.17Z&interval=4000', # Season 16 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-17T15:59:50.17Z&interval=4000', # Season 16 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-19T15:59:50.17Z&interval=4000', # Season 17 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-04-24T15:59:50.17Z&interval=4000', # Season 17 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-05-10T15:59:50.17Z&interval=4000', # Season 18 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-05-15T15:59:50.17Z&interval=4000', # Season 18 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-05-17T15:59:50.17Z&interval=4000', # Season 19 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-05-22T15:59:50.17Z&interval=4000', # Season 19 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-06-14T15:59:50.17Z&interval=4000', # Season 20 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-06-19T15:59:50.17Z&interval=4000', # Season 20 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-06-21T15:59:50.17Z&interval=4000', # Season 21 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-06-26T15:59:50.17Z&interval=4000', # Season 21 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-06-28T15:59:50.17Z&interval=4000', # Season 22 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-07-03T15:59:50.17Z&interval=4000', # Season 22 semifinals
+    'https://api.sibr.dev/replay/v1/replay?from=2021-07-19T15:59:50.17Z&interval=4000', # Season 23 start
+    'https://api.sibr.dev/replay/v1/replay?from=2021-07-24T15:59:50.17Z&interval=4000', # Season 23 semifinals
+]
 
 team_map = {
     '747b8e4a-7e50-4638-a973-ea7950a3e739': 1,
@@ -237,140 +265,148 @@ async def main():
     counter_playerget = 0
 
     # Get data stream and process it constantly
-    # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-06-16T00:59:50.17Z&interval=1500'): # Faster
-    async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-06-15T05:59:50.17Z&interval=3000'): # Fast
-    # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-06-16T00:59:50.17Z&interval=4000'): # Normal?
-    # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2020-10-05T16:31:50.17Z&interval=1500'): # Crowvertime
-    # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-05-20T11:44:50.17Z&interval=1500'): # Drumsolo
-        if event.get('games',{}).get('schedule'):
-            # Get team lineups
-            if event.get('leagues'):
-                teams = event.get('leagues',{}).get('teams')
-            if not teams:
-                continue
-            teams = [team for team in teams if team['id'] in team_map.keys()] # Remove teams not playing
-            player_ids = [player for team in teams for player in team['lineup']]
-            if (set(player_ids) != player_ids_cache) or (counter_playerget >= 100):
-                players = mikedb.get_player(player_ids)
-                player_ids_cache = set(player_ids)
-                counter_playerget = 0
-            counter_playerget += 1
-            player_map = {} # Used later as well
-            for player in players.values():
-                player_map[player['id']] = player.get('unscatteredName',player.get('name'))
-            team_lineups = {}
-            for team in teams:
-                lineup = team['lineup']
-                lineup_names = [player_map[player] for player in lineup]
-                while len(lineup_names) < 16:
-                    lineup_names.append('')
-                team_lineups[team['id']] = lineup_names
+    for timestamp in timestamps:
+        async for event in stream_events(url=timestamp):
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-06-16T00:59:50.17Z&interval=1500'): # Faster
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-03-01T15:59:50.17Z&interval=4000'): # Fast
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-07-30T15:59:50.17Z&interval=4000'): # Fast
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-06-16T00:59:50.17Z&interval=4000'): # Normal?
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2020-10-05T16:31:50.17Z&interval=1500'): # Crowvertime
+        # async for event in stream_events(url='https://api.sibr.dev/replay/v1/replay?from=2021-05-20T11:44:50.17Z&interval=1500'): # Drumsolo
+            if event.get('games',{}).get('sim',{}).get('phase',0) in [10,12]:
+                break
+            if event.get('games',{}).get('schedule'):
+                # Get team lineups
+                if event.get('leagues'):
+                    teams = event.get('leagues',{}).get('teams')
+                if not teams:
+                    continue
+                teams = [team for team in teams if team['id'] in team_map.keys()] # Remove teams not playing
+                player_ids = [player for team in teams for player in team['lineup']]
+                if (set(player_ids) != player_ids_cache) or (counter_playerget >= 100):
+                    players = mikedb.get_player(player_ids)
+                    player_ids_cache = set(player_ids)
+                    counter_playerget = 0
+                counter_playerget += 1
+                player_map = {} # Used later as well
+                for player in players.values():
+                    player_map[player['id']] = player.get('unscatteredName',player.get('name'))
+                team_lineups = {}
+                for team in teams:
+                    lineup = team['lineup']
+                    lineup_names = [player_map[player] for player in lineup]
+                    while len(lineup_names) < 16:
+                        lineup_names.append('')
+                    team_lineups[team['id']] = lineup_names
 
-            # Initialize bulk data structures
-            payload_dict = {}
-            weathers = ['' for ngames in range(12)]
-            feeds = ['' for ngames in range(12)]
-            feed_types = [0 for ngames in range(12)]
-            away_ids = ['' for ngames in range(12)]
-            home_ids = ['' for ngames in range(12)]
-            base5s = ['' for ngames in range(12)]
-            balls_all = ['' for ngames in range(12)]
-            strikes_all = ['' for ngames in range(12)]
-            outs_all = ['' for ngames in range(12)]
-            finalized_all = ['' for ngames in range(12)]
-            battings = ['' for ngames in range(12)]
-            away_scores = ['' for ngames in range(12)]
-            home_scores = ['' for ngames in range(12)]
+                # Initialize bulk data structures
+                payload_dict = {}
+                weathers = ['' for ngames in range(12)]
+                feeds = ['' for ngames in range(12)]
+                feed_types = [0 for ngames in range(12)]
+                away_ids = ['' for ngames in range(12)]
+                home_ids = ['' for ngames in range(12)]
+                base5s = ['' for ngames in range(12)]
+                balls_all = ['' for ngames in range(12)]
+                strikes_all = ['' for ngames in range(12)]
+                outs_all = ['' for ngames in range(12)]
+                finalized_all = ['' for ngames in range(12)]
+                battings = ['' for ngames in range(12)]
+                away_scores = ['' for ngames in range(12)]
+                home_scores = ['' for ngames in range(12)]
 
-            # Get games
-            games = event['games']['schedule']
-            # Reset inning scores and set a new fixed order of games if it's a new day
-            if event['games']['sim']['day'] != today:
-                innings_away = [['','','','','','','','',''] for ngames in range(12)]
-                innings_home = [['','','','','','','','',''] for ngames in range(12)]
-                today = event['games']['sim']['day']
-                games_map = {}
-                for i_game,game in enumerate(games):
-                    games_map[game['id']] = i_game
+                # Get games
+                games = event['games']['schedule']
+                # Reset inning scores and set a new fixed order of games if it's a new day
+                if event['games']['sim']['day'] != today:
+                    innings_away = [['','','','','','','','',''] for ngames in range(12)]
+                    innings_home = [['','','','','','','','',''] for ngames in range(12)]
+                    today = event['games']['sim']['day']
+                    games_map = {}
+                    for i_game,game in enumerate(games):
+                        games_map[game['id']] = i_game
 
-            # Loop over games
-            for game in games:
-                i_game = games_map[game['id']]
+                # Loop over games
+                for game in games:
+                    i_game = games_map[game['id']]
 
-                # Get baserunners
-                bases = game['basesOccupied']
-                baserunners = game['baseRunners']
-                baserunner_names = [player_map.get(baserunner,'A GHOST!') for baserunner in baserunners]
-                bases_all = ['','','','']
-                for base,baserunner_name in zip(bases,baserunner_names):
-                    bases_all[base] = baserunner_name
-                if game['topOfInning']:
-                    away_baserunners = bases_all
-                    home_baserunners = ['','','','']
-                else:
-                    home_baserunners = bases_all
-                    away_baserunners = ['','','','']
-                # Get home team
-                home_bases = home_baserunners
-                home_id = game['homeTeam']
-                home_ids[i_game] = home_id
-                home_pitcher = game['homePitcherName']
-                home_batter = player_map.get(game['homeBatter'],'') if not game['topOfInning'] else ''
-                # Get away team
-                away_bases = away_baserunners
-                away_id = game['awayTeam']
-                away_ids[i_game] = away_id
-                away_pitcher = game['awayPitcherName']
-                away_batter = player_map.get(game['awayBatter'],'') if game['topOfInning'] else ''
-                # Add to payload
-                payload_dict[team_map[home_id]] = [home_pitcher]+[home_batter]+home_bases+team_lineups[home_id]
-                payload_dict[team_map[away_id]] = [away_pitcher]+[away_batter]+away_bases+team_lineups[away_id]
+                    # Get baserunners
+                    bases = game['basesOccupied']
+                    baserunners = game['baseRunners']
+                    baserunner_names = [player_map.get(baserunner,'A GHOST!') for baserunner in baserunners]
+                    bases_all = ['','','','']
+                    for base,baserunner_name in zip(bases,baserunner_names):
+                        if base <= 3: # Prevents problems with bases beyond the fifth base.
+                            bases_all[base] = baserunner_name
+                    if game['topOfInning']:
+                        away_baserunners = bases_all
+                        home_baserunners = ['','','','']
+                    else:
+                        home_baserunners = bases_all
+                        away_baserunners = ['','','','']
+                    # Get home team
+                    home_bases = home_baserunners
+                    home_id = game['homeTeam']
+                    home_ids[i_game] = home_id
+                    home_pitcher = game['homePitcherName']
+                    home_batter = player_map.get(game['homeBatter'],'') if not game['topOfInning'] else ''
+                    # Get away team
+                    away_bases = away_baserunners
+                    away_id = game['awayTeam']
+                    away_ids[i_game] = away_id
+                    away_pitcher = game['awayPitcherName']
+                    away_batter = player_map.get(game['awayBatter'],'') if game['topOfInning'] else ''
+                    # Add to payload
+                    payload_dict[team_map[home_id]] = [home_pitcher]+[home_batter]+home_bases+team_lineups[home_id]
+                    payload_dict[team_map[away_id]] = [away_pitcher]+[away_batter]+away_bases+team_lineups[away_id]
 
-                # Get game data
-                if game['topOfInning']:
-                    batting = away_id
-                    if game['inning'] in range(9):
-                        innings_away[i_game][game['inning']] = game['awayScore'] - sum([innings_away[i_game][inning] for inning in range(game['inning']) if innings_away[i_game][inning] != '']) #game['halfInningScore'] # halfInning* fields are bugged for home team =/
-                else:
-                    batting = home_id
-                    if game['inning'] in range(9):
-                        innings_home[i_game][game['inning']] = game['homeScore'] - sum([innings_home[i_game][inning] for inning in range(game['inning']) if innings_home[i_game][inning] != '']) #game['halfInningScore'] # halfInning* fields are bugged for home team =/
-                weathers[i_game] = game['weather']
-                feeds[i_game] = game['lastUpdate'].replace('\n',' ')
-                feed_types[i_game] = feedtyper(game['lastUpdate'])
-                base5s[i_game] = True if game['homeBases']==5 else False
-                balls_all[i_game] = game['atBatBalls']
-                strikes_all[i_game] = game['atBatStrikes']
-                outs_all[i_game] = game['halfInningOuts']
-                finalized_all[i_game] = game['finalized']
-                battings[i_game] = batting
-                away_scores[i_game] = game['awayScore']
-                home_scores[i_game] = game['homeScore']
+                    # Get game data
+                    if game['topOfInning']:
+                        batting = away_id
+                        if game['inning'] in range(9):
+                            innings_away[i_game][game['inning']] = game['awayScore'] - sum([innings_away[i_game][inning] for inning in range(game['inning']) if innings_away[i_game][inning] != '']) #game['halfInningScore'] # halfInning* fields are bugged for home team =/
+                    else:
+                        batting = home_id
+                        if game['inning'] in range(9):
+                            innings_home[i_game][game['inning']] = game['homeScore'] - sum([innings_home[i_game][inning] for inning in range(game['inning']) if innings_home[i_game][inning] != '']) #game['halfInningScore'] # halfInning* fields are bugged for home team =/
+                    weathers[i_game] = game['weather']
+                    feeds[i_game] = game['lastUpdate'].replace('\n',' ')
+                    feed_types[i_game] = feedtyper(game['lastUpdate'])
+                    base5s[i_game] = True if game['homeBases']==5 else False
+                    balls_all[i_game] = game['atBatBalls']
+                    strikes_all[i_game] = game['atBatStrikes']
+                    outs_all[i_game] = game['halfInningOuts']
+                    finalized_all[i_game] = game['finalized']
+                    battings[i_game] = batting
+                    away_scores[i_game] = game['awayScore']
+                    home_scores[i_game] = game['homeScore']
 
-            # Assemble all data for this tick
-            payload = []
-            for row in range(1,25):
-                payload.append(payload_dict.get(row,['']))
-            payload.append([event.get('games').get('sim').get('season')+1,event.get('games').get('sim').get('day')+1,'','','','','','','','','',''])
-            payload.append(weathers)
-            payload.append(feeds)
-            payload.append(feed_types)
-            payload.append(away_ids)
-            payload.append(home_ids)
-            payload.append(base5s)
-            payload.append(balls_all)
-            payload.append(strikes_all)
-            payload.append(outs_all)
-            payload.append(finalized_all)
-            payload.append(battings)
-            payload.append(away_scores)
-            payload.append(home_scores)
-            for inning in range(9):
-                payload.append([game[inning] for game in innings_away])
-            for inning in range(9):
-                payload.append([game[inning] for game in innings_home])
-            # Upload Sheets with this tick
-            worksheet.update('D2:Y57', payload)
+                # Assemble all data for this tick
+                payload = []
+                for row in range(1,25):
+                    payload.append(payload_dict.get(row,['']))
+                payload.append([event.get('games').get('sim').get('season')+1,event.get('games').get('sim').get('day')+1,'','','','','','','','','',''])
+                payload.append(weathers)
+                payload.append(feeds)
+                payload.append(feed_types)
+                payload.append(away_ids)
+                payload.append(home_ids)
+                payload.append(base5s)
+                payload.append(balls_all)
+                payload.append(strikes_all)
+                payload.append(outs_all)
+                payload.append(finalized_all)
+                payload.append(battings)
+                payload.append(away_scores)
+                payload.append(home_scores)
+                for inning in range(9):
+                    payload.append([game[inning] for game in innings_away])
+                for inning in range(9):
+                    payload.append([game[inning] for game in innings_home])
+                # Upload Sheets with this tick
+                worksheet.update('G2:AB57', payload)
+
 
 # Execution starts here.
-asyncio.run(main())
+while True:
+    asyncio.run(main())
